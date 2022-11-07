e15788063a30a1533fbe5e482dfb18724652074a
24mesquita <mesquita.rm17@gmail.com>
2022-11-07 20:19:55 +0000

    fix:interests

--- src/Interests/index.js
+++ src/Interests/index.js
@@ -2,7 +2,9 @@ import * as React from 'react';
 import './index.css';
 import { useStyles } from "./styles";
 import img from '../props/Foto.jpeg';
-import develop from '../props/web-develoment.jpeg';
+import develop from '../props/web-devlopement.jpeg';
+import gym from '../props/gym.jpeg';
+import horse from '../props/horse.jpeg';
 
 
 
@@ -30,29 +32,69 @@ function Home() {
     </div>
     
   </div>
-  <div  className='interest-container2'><div className='interest-title'>Developement</div>
-        <h4 className='interest-description'>Develop, principaly web Developement is one of the things that I love to do. </h4>
-        <img  className='interest-img' src={develop}></img>
+    <div className='interests'>
+      <div className='interests-title'>
+        Interests
+      </div>
+      <div>
+      <div className='interests-content'>
+            <div className='imterests-container-img'><img className='imterests-img'src={develop}></img></div>
+            <div className='interests-text'>
+                <div className='interests-text-title'>
+                  <h1>Developement</h1>
+                </div>
+                <div className='interests-text-description'>
+                    <p>Develop, principaly web Developement is one of the things that I love to do.</p>
+                </div>
+            </div>
+      </div>
 
-  </div>
-      <div className='interest-container'><div className='interest-title'>Gym</div>
-      <h4 className='interest-description'>This is what saved my life, since I discovered the Gym, I got a new lifestyle, I started to feel my self better and my confidence its better like never happened, for about 2 years that I'm in love with lifting weights. </h4>
-      
+      <div>
+      <div className='interests-content'>
+            <div className='imterests-container-img'><img className='imterests-img'src={gym}></img></div>
+            <div className='interests-text'>
+                <div className='interests-text-title'>
+                  <h1>Gym</h1>
+                </div>
+                <div className='interests-text-description'>
+                    <p>This is what saved my life, since I discovered the Gym, I got a new lifestyle, I started to feel my self better and my confidence its better like never happened, for about 2 years that I'm in love with lifting weights.</p>
+                </div>
+            </div>
       </div>
-      <div className='interest-container2'> <div className='interest-title'>Horse Riding</div>
-      <h4 className='interest-description'>Since I was a kid I love horses, I had my first horse at 7 yeas old, and I started riding at the same age, thats pure therapy, its only you and the connection with the horse. </h4>
-      
+    </div>
+
+    <div>
+      <div className='interests-content'>
+            <div className='imterests-container-img'><img className='imterests-img'src={horse}></img></div>
+            <div className='interests-text'>
+                <div className='interests-text-title'>
+                  <h1>Horse Riding</h1>
+                </div>
+                <div className='interests-text-description'>
+                    <p>Since I was a kid I love horses, I had my first horse at 7 yeas old, and I started riding at the same age, thats pure therapy, its only you and the connection with the horse.</p>
+                </div>
+            </div>
       </div>
-      <div className='interest-container'> <div className='interest-title'>Hang out with friends</div>
+    </div>
+
+
+
+
+
+            
+
+
+        </div>
+
+
+
+       
+
+    </div>
+    </div>
       
-      
-      
-      </div>
-      <div className='interest-container2'> <div className='interest-title'>Travel</div> 
-      
-      
-      </div>
-	</div>
+
+
 
 
 		
